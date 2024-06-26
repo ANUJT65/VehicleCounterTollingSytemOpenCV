fd9eac027bc92f9830801f4401f741868078c2a4
Anuj Tadkase <123918593+ANUJT65@users.noreply.github.com>
2024-06-26 21:58:31 +0000

    newmachinelearning engineer in the making

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from re import DEBUG
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
 def helloworld():
-  return "helloworld"
+  return render_template("home.html")
   print(__name__)
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
