b3072304dcf6d8bd027d2bef9322c0bcc1f743cd
Anuj Tadkase <123918593+ANUJT65@users.noreply.github.com>
2024-06-28 23:21:03 +0000

    new_updated

--- app.py
+++ app.py
@@ -1,9 +1,39 @@
 from re import DEBUG
-from flask import Flask, render_template
+from flask import Flask, json, render_template,jsonify
 app = Flask(__name__)
+
+JOBS=[
+  {
+    'id':1,
+    'title':'Data Analyst',
+    'location':'Bengluru,India',
+    'salary':'Rs.10,00,000'
+  },
+   {
+    'id':2,
+    'title':'MLE',
+    'location':'PUNE,India',
+    'salary':'Rs.15,00,000'
+   },
+   {
+    'id':3,
+    'title':'FULL STACK',
+    'location':'Noida,India',
+     'salary':'Rs.15,00,000'
+   
+   }
+]
+
+
+
 @app.route("/")
 def helloworld():
-  return render_template("home.html")
-  print(__name__)
+  return render_template("home.html", jobs=JOBS)
+
+
+@app.route("/api/jobs")
+def listjobs():
+  return jsonify(JOBS)
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
